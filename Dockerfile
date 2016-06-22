615a87e2d4357027900b3351277fb9d4e2152a2b
Marcel Großmann <whatever4711@gmail.com>
2016-06-22 17:31:04 +0200

    Self Generating Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ MAINTAINER Marcel Grossmann <whatever4711@gmail.com>
 
 COPY PyTex.py ${dir}/
 ADD pythonlib ${dir}/pythonlib
-COPY metainfo.csv ${dir}/
 COPY template.tex ${dir}/
 ADD ExampleTemplate ${dir}/ExampleTemplate
 WORKDIR ${dir}
 
 ENTRYPOINT ["/bin/sh", "-c"]
+CMD ["pytex/PyTex.py", "-h"]
--- pythonlib/Docker.py
+++ pythonlib/Docker.py
@@ -9,7 +9,7 @@
     Python Version: 3.4
 '''
 
-from os import getuid, getgid, getcwd
+from os import getuid, getgid, getcwd, path
 from subprocess import call, Popen, PIPE
 from threading import Thread
 from json import dumps
@@ -37,7 +37,9 @@ class Builder():
         # Check if container exists
         (output, err) = Popen(["docker", "images"], stdout=PIPE).communicate()
         lines = output.decode('utf-8')
-        if str(kwargs['containerName']) not in lines:
+        if not path.isfile("Dockerfile"):
+            self.writeDockerfile(str(kwargs['folder']))
+        if not self.containerExists(str(kwargs['containerName'])):
             call(['docker', 'build', '-t', kwargs['containerName'], '.'], cwd=".")
         self.data = kwargs
 
@@ -48,3 +50,22 @@ class Builder():
             dock.start()
 
         return threads
+
+    def containerExists(self, containerName):
+        (output, err) = Popen(["docker", "images"], stdout=PIPE).communicate()
+        lines = output.decode('utf-8')
+        return (False, True)[containerName in lines]
+
+    def writeDockerfile(self, templateFolder):
+        lines = []
+        lines.append("FROM unibaktr/dock-tex:python")
+        lines.append("MAINTAINER Marcel Grossmann <whatever4711@gmail.com>\n")
+        lines.append("COPY PyTex.py ${dir}/")
+        lines.append("ADD pythonlib ${dir}/pythonlib")
+        lines.append("COPY template.tex ${dir}/")
+        lines.append("ADD %s ${dir}/%s" % (templateFolder, templateFolder))
+        lines.append("WORKDIR ${dir}\n")
+        lines.append("ENTRYPOINT [\"/bin/sh\", \"-c\"]")
+        lines.append("CMD [\"pytex/PyTex.py\", \"-h\"]")
+        with open("Dockerfile", 'w', encoding='utf-8') as dockerfile:
+            dockerfile.write('\n'.join(lines))
